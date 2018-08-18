e6375c4435f8c820ba82024a7bf277704a774ee7
drilla <drillerok@gmail.com>
2018-08-18 16:25:06 +0300

    fix css and layout

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{}
---
+++ web/assets/js/common.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $(document).ready(function() {
+        'use strict';
+
+        /**
+         * стандартная упаковка
+         */
+        (function () {
+
+        }());
+
+    });
+})(jQuery);
