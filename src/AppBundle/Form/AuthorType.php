344dfa56dfbd95f717a5086b2bcad78d73cbd618
drilla <drillerok@gmail.com>
2018-08-19 23:07:08 +0300

    fix birth date

--- src/AppBundle/Form/AuthorType.php
+++ src/AppBundle/Form/AuthorType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use AppBundle\Entity;
 use AppBundle\Helper;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,7 @@ class AuthorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('name')
-            ->add('birthDate')
+            ->add('birthDate', BirthdayType::class)
             ->add('gender', ChoiceType::class, [
                 'choices' => Entity\Author::GENDERS,
                 'choice_label' => function($value) {
